d52dcf0451c6be2f27a0dbf2e74d598f885e992c
yellowos <2442605351@qq.com>
2017-07-29 22:23:05 +0800

    add precise divider

--- uart/testbench.v
+++ uart/testbench.v
@@ -1,1 +1,44 @@
+`timescale 1ns/1ns
+module precise_divider
+#(
+	paramater DEVIDE_CNT = 332'd6597070;
+)
+(
+	input clk,
+	input rst_n,
 
+	output divide_clk,
+	output divide_clken
+);
+
+	reg [31 : 0] cnt;
+	always@(posedge clk or negedge rst_n) begin
+		if(!rst_n)
+			cnt <= 0;
+		else
+			cnt <= cnt + DEVIDE_CNT;
+	end
+
+
+	reg cnt_equal;
+	always@(posedge clk or negedge rst_n) begin
+		if(!rst_n)
+			cnt <= 0;
+		else if(cnt < 32'h7FFF_FFFF)
+			cnt_equal <= 0;
+		else 
+			cnt_equal <= 1;
+	end
+
+
+	reg cnt_equal_r
+	always@(posedge clk or negedge rst_n) begin
+		if(!rst_n)
+			cnt_equal_r <= 0;
+		else
+			cnt_equal_r <= cnt_equal;
+	end
+
+	assign divide_clken = (~cnt_equal_r & cnt_equal) ? 1'b1 : 1'b0;
+	assign divide_clk = clk_equal_r;
+endmodule
